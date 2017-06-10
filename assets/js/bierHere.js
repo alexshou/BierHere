87218567d93e126b4681f279d046ace6d75d4e59
Cody <cody@posteo.de>
2017-06-10 13:26:18 -0400

    fix missing curly bracket

--- assets/js/bierHere.js
+++ assets/js/bierHere.js
@@ -31,12 +31,13 @@ function findByCity(city, state) {
         console.log(brewery)
       })
     })
-  
+}
+
 function initMap() {
-// Create a map object and specify the DOM element for display.
-	var map = new google.maps.Map(document.getElementById('map'), {
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 41.397, lng: 81.644},
     scrollwheel: false,
     zoom: 8
-    });
+  });
 }
