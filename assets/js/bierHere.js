f32a1b3a13c0087325fefa577250bed3cd8a26f8
Cody <cody@posteo.de>
2017-06-13 19:46:19 -0400

    make api query use options object

--- assets/js/bierHere.js
+++ assets/js/bierHere.js
@@ -1,40 +1,30 @@
-// gets a list of all breweries in a given city
-// ex. findByCity('New York', 'New York')
-// Abbreviated state name sometimes works, but not always so avoid it.
-// ex. findByCity('New York', 'NY') -> no
-// ex findByCity('Kansas City', 'MO') -> yes
-var findByCity = queryAPIBy.bind(undefined, 'city')
-
-// gets a list of all breweries in a given zipcode
-// ex. findByZipcode(64108)
-var findByZipcode = queryAPIBy.bind(undefined, 'zip')
-
-// please don't use this directly!!!
-function queryAPIBy(type, options) {
+// Query by city: queryAPIBy({city: 'akron', state: 'ohio'})
+// query by zipcode: queryAPIBy({zip: '44113'})
+function queryAPIBy(options) {
   var url = '/locations/?key=ef6233841a88d451b69d43089bd4b81a'
+  var defaults = {
+    zip: null,
+    city: null,
+    state: null,
+  }
 
-  // if queried by  zipcode, we get the second thing from the
-  // argument array (type is the first) and set params specifically for
-  // that params is used later in the axios get request
-  if (type === 'zip') {
-    var zipcode = arguments[1]
+  var options = Object.assign(defaults, options)
+
+  if (options.zip) {
     var params = {
-      postalCode: zipcode
+      postalCode: options.zip
     }
   }
 
-  // if queried by city, we get the second and third thing from the
-  // argument array (type is the first),
-  if (type === 'city') {
-    var city = arguments[1]
-    var state = arguments[2]
+  if (options.city && options.state) {
     var params = {
-      locality: city,
-      region: state
+      locality: options.city,
+      region: options.state
     }
   }
 
   var locations = []
+
   axios.get(url, {
     // we use cors-anywhere here to get around same origin restriction that
     // breweryDB has on their API
@@ -61,4 +51,8 @@ function initMap() {
     scrollwheel: false,
     zoom: 8
   });
+}
+
+function makeLocationList(apiCall, callback) {
+
 }
