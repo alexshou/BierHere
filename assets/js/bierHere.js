d228574a32d5db4066ed55cfd1227bf68b8ee558
Xingxian Shou <Xingxian Shou>
2017-06-13 19:20:28 -0400

    google map

--- assets/js/bierHere.js
+++ assets/js/bierHere.js
@@ -16,11 +16,12 @@ $("#add-search").on("click", function() {
     event.preventDefault();
     var zip = $("#zip-input").val().trim();
     var city = $("#city-input").val().trim();
-
+    var state = $("#state-input").val().trim();
     
     database.ref().push({
       zip: zip,
-      city: city
+      city: city,
+      state: state
 
       //dataAdded: firebase.database.ServerValue.TIMESTAMP
     })
@@ -33,33 +34,77 @@ var locArray = [];
 locArray.push([41.34, -80.12]);
 locArray.push([41.0, -81.2]);
 console.log(locArray);
-console.log(locArray[0][0]);
-console.log(locArray[0][1]);
-console.log(locArray[1][0]);
-console.log(locArray[1][1]);
+//console.log(locArray[0][0]);
+//console.log(locArray[0][1]);
+//console.log(locArray[1][0]);
+//console.log(locArray[1][1]);
+
+// var locs = findByZipcode(44113);
+
+//console.log(locs[0].lat);
+//console.log(locs[0].lon);
+//console.log(locs[0].name);
+// gets a list of all breweries in a given zipcode
+
+// Multiple Markers, need to be replaced by API results from breweryDB
+var markers = [
+  ['Nano Brew Cleveland', 41.4860133, -81.7046873],
+  ['Great Lakes Brewing Company', 41.484446, -81.704436]
+
+ // ['Nano Brew Cleveland', locs[0].lat, locs[0].lon],
+ // ['Great Lakes Brewing Company', locs[1].lat, locs[1].lon]
+];
+
+console.log(markers);
 
 function initMap() {
-// Create a map object and specify the DOM element for display.
-//the Latitude and longitude for myCenter should be coppresponding to the zip code used entered
-    var mapBier = document.getElementById("map");
- 	var mapOptions = {
-    center: new google.maps.LatLng(locLat, locLng),
-    zoom: 7,
-    panControl: true,
-    zoomControl: true,
-    mapTypeControl: true,
-    scaleControl: true,
-    streetViewControl: true,
-    overviewMapControl: true,
-    rotateControl: true   
-  };
-  var marker = new google.maps.Marker({position:mapOptions.center});
-  var map = new google.maps.Map(mapBier, mapOptions);
-  marker.setMap(map);
-  google.maps.event.addListener(map, 'click', function(event) {
-    placeMarker(map, event.latLng);
-  });
 
+
+    var map;
+    var bounds = new google.maps.LatLngBounds();
+    var mapOptions = {
+        mapTypeId: 'roadmap'
+    };
+                    
+    // Display a map on the page
+    map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    map.setTilt(45);
+        
+                        
+    // Info Window Content
+    var infoWindowContent = [
+        ['<div class="info_content">' +
+        '<h3>Nano Brew Cleveland</h3>' +
+        '<p> You like it! </p>' + '</div>'],
+        ['<div class="info_content">' +
+        '<h3>Great Lakes Brewing Company</h3>' +
+        '<p>The No.1 beer in Ohio</p>' + '</div>']
+    ];
+        
+    // Display multiple markers on a map
+    var infoWindow = new google.maps.InfoWindow(), marker, i;
+    
+    // Loop through our array of markers & place each one on the map  
+    for( i = 0; i < markers.length; i++ ) {
+        var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
+        bounds.extend(position);
+        marker = new google.maps.Marker({
+            position: position,
+            map: map,
+            title: markers[i][0]
+        });
+        
+        // Allow each marker to have an info window    
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infoWindow.setContent(infoWindowContent[i][0]);
+                infoWindow.open(map, marker);
+            }
+        })(marker, i));
+
+        // Automatically center the map fitting all markers on the screen
+        map.fitBounds(bounds);
+    }
 
 
 //  google.maps.event.addListener(map, 'click', function(event) {
@@ -85,7 +130,8 @@ var findByZipcode = queryAPIBy.bind(undefined, 'zip')
 
 // please don't use this directly!!!
 function queryAPIBy(type, options) {
-  var url = '/locations/?key=ef6233841a88d451b69d43089bd4b81a'
+
+     var url = '/locations/?key=ef6233841a88d451b69d43089bd4b81a'
 
   // if queried by  zipcode, we get the second thing from the
   // argument array (type is the first) and set params specifically for
@@ -129,6 +175,6 @@ function queryAPIBy(type, options) {
 }
 
 
+//console.log(locs);
+//console.log("locs.length: " + locs.length);
 
-
- 
