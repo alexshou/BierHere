ed92b62ed5b2a33b82d7673895d59bf8cabe1739
Cody <cody@posteo.de>
2017-06-17 11:19:54 -0400

    style

--- assets/js/bierHere.js
+++ assets/js/bierHere.js
@@ -295,6 +295,6 @@ Vue.component('beer-item', {
   },
   template: '<div>' +
     '<li v-on:click="toggle"> {{ beer.name }}</li>' +
-    '<p v-show="show"> {{ beer.description }} </p>' +
+    '<p v-show="show" class="beer"> {{ beer.description }} </p>' +
     '</div>'
 })
